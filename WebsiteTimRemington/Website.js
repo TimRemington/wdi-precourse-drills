07438d1a63c4f52fe0e02ef452f5a422bf35c27e
TimRemington <36176921+TimRemington@users.noreply.github.com>
2018-08-23 23:13:41 -0600

    Add files via upload
    Here is my website.

---
+++ WebsiteTimRemington/Website.js
@@ -0,0 +1,5 @@
+// javascript Personal Website
+
+// (X) links, images with scrolling, (X) text to about page
+// (X) reference to 2 pages (html), (X) CSS, at least 1 DOM manipulation
+// personal page
